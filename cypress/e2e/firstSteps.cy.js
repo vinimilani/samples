4bcec7d036a9f96cec85b734318f2e260fb38f20
QA-Vinicius <vinicius@upoutsourcing.com>
2024-03-26 16:59:14 -0300

    refactor(e2e): storing selectors in variables

--- cypress/e2e/firstSteps.cy.js
+++ cypress/e2e/firstSteps.cy.js
@@ -1,11 +1,15 @@
+import { selectors } from '../fixtures/selectors' //importing the menuSelector from the selectors file
+
 describe ('Basic E2E Testing', () => {
     it('First steps on cypress docs page', () => {
+        cy.intercept('GET', 'https://docs.cypress.io/guides/overview/why-cypress/').as('PageLoaded') //intercepting an API call to wait
         cy.visit('/') //we set the test URL on the config file, so we can just use '/'
         cy.get('h1').should('contain', 'Why Cypress?').and('be.visible') //visibility and text assertions
-        cy.get('.navbar__link--active').should('have.attr', 'href') //attribute assertion
-        cy.wait(2000)
-        cy.get('.icon-light-transparent').click() //opening the search bar
-        cy.get('#docsearch-input').type('first steps', {delay: 200}) //delaying to simulate a real user typing
-        cy.get('.DocSearch-Reset').click() //clearing the search bar
+        cy.get(selectors.headerMenu).should('have.attr', 'href') //attribute assertion
+        cy.wait('@PageLoaded') //waiting for the API call to make sure that the content loaded
+        cy.wait(3000) //this wait is just to make sure that the page is fully loaded
+        cy.get(selectors.searchIcon).click() //opening the search bar
+        cy.get(selectors.searchBar).type('first steps', {delay: 200}) //delaying to simulate a real user typing
+        cy.get(selectors.searchX).click() //clearing the search bar
         });
 })
---
+++ cypress/fixtures/selectors.js
@@ -0,0 +1,6 @@
+export const selectors = { //storing selectors on variables makes it easier to maintain the code
+    headerMenu :'.navbar__link--active',
+    searchIcon: '.icon-light-transparent',
+    searchBar: '#docsearch-input',
+    searchX: '.DocSearch-Reset'
+}
