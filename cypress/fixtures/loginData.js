ec6acdf9f1e854a13d8c9650fd0fb8667ee7f31e
vinissues <viniciusmilani.bb@hotmail.com>
2024-04-03 11:56:59 -0300

    test: changing user on github

--- cypress/fixtures/loginData.js
+++ cypress/fixtures/loginData.js
@@ -2,5 +2,5 @@ export const LoginData = {
     validUser: {
         username: 'tomsmith',
         password: 'SuperSecretPassword!' //you can also store the password in a file that is not tracked by git
-    },
+    }
 }
